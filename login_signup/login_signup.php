e559b0764e4fc225caa2b3d94dbd79e20cfe3ca4
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-05-03 01:52:49 +0300

    Update login_signup.php

--- login_signup/login_signup.php
+++ login_signup/login_signup.php
@@ -2,7 +2,7 @@
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>Login | Sign Up</title>
+  <title>Login | Register</title>
   <head>
   <meta charset="UTF-8">
   <title>Gene-Disease Association</title>
@@ -20,16 +20,16 @@
         Login
         <span class="underline"></span>
       </button>
-      <form class="form form-login">
+      <form class="form form-login" action="connection1.php" method="POST"> <!-- Action specifies the location to transfer the submitted user information; Connection 1 is for login-->
         <fieldset>
           <legend>Please, enter your email and password for login.</legend>
           <div class="input-block">
             <label for="login-email">E-mail</label>
-            <input id="login-email" type="email" required>
+            <input id="login-email" type="email" name="email" required placeholder="hello@world.com">
           </div>
           <div class="input-block">
             <label for="login-password">Password</label>
-            <input id="login-password" type="password" required>
+            <input id="login-password" type="password" name="password" required placeholder="Password">
           </div>
         </fieldset>
         <button type="submit" class="btn-login">Login</button>
@@ -37,23 +37,23 @@
     </div>
     <div class="form-wrapper">
       <button type="button" class="switcher switcher-signup">
-        Sign Up
+        Register Doctor
         <span class="underline"></span>
       </button>
-      <form class="form form-signup">
+      <form class="form form-signup" action="connection2.php" method="POST"> <!-- Connection 2 is to register-->
         <fieldset>
           <legend>Please, enter your email, password and password confirmation for sign up.</legend>
           <div class="input-block">
             <label for="signup-email">E-mail</label>
-            <input id="signup-email" type="email" required>
+            <input id="signup-email" type="email" name="email" required placeholder="hello@world.com">
           </div>
           <div class="input-block">
             <label for="signup-password">Password</label>
-            <input id="signup-password" type="password" required>
+            <input id="signup-password" type="password" name="password" required placeholder="Password">
           </div>
           <div class="input-block">
             <label for="signup-password-confirm">Confirm password</label>
-            <input id="signup-password-confirm" type="password" required>
+            <input id="signup-password-confirm" type="password" required placeholder="Password">
           </div>
         </fieldset>
         <button type="submit" class="btn-signup">Continue</button>
