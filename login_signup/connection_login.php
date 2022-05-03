a17352525301f9c9f485e1fc5a1f84e0b05b6ac5
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-05-03 03:16:12 +0300

    Add files via upload

---
+++ login_signup/connection_login.php
@@ -0,0 +1,21 @@
+<?php
+	session_start();
+
+	$connect = mysqli_connect('localhost', 'root', '');
+	mysqli_select_db($connect, 'gene-disease-association-db');
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	//$error = "Invalid Email or Password.";
+	$select = "SELECT * FROM doctors WHERE email='$email' AND password='$password'";
+	$output = mysqli_query($connect, $select);
+	$count = mysqli_num_rows($output);
+
+	if($count == 1){
+		$_SESSION['email']=$email; 
+		header('location:../html/index.html');
+	}
+	else{
+		//$_SESSION["error"] = $error;
+		header('location:login_signup.php');
+	}
+?>
