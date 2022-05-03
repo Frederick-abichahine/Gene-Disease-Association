0b5d5eb32566aaea7a8c7a2a62f7f2f82474afce
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-05-03 13:46:08 +0300

    Update connection_register.php

--- login_signup/connection_register.php
+++ login_signup/connection_register.php
@@ -11,17 +11,33 @@
 	$specialty = $_POST['specialty'];
 	$password = $_POST['password'];
 
-	$checker = "SELECT * FROM doctors WHERE email='$email'";
-	$result = mysqli_query($connect, $checker);
-	$num = mysqli_num_rows($result);
+	$checker1 = "SELECT * FROM doctors WHERE email='$email'";
+	$result1 = mysqli_query($connect, $checker1);
+	$num1 = mysqli_num_rows($result1);
 
-	if($num == 1){
+	$checker2 = "SELECT * FROM doctors WHERE mobile_number='$mobile_number'";
+	$result2 = mysqli_query($connect, $checker2);
+	$num2 = mysqli_num_rows($result2);
+
+	if ($num1 == 1) {
 		echo "Email Already In Use.";
 	}
-
-	else{
-
-		//add doctor using INSERT INTO query
-		//edit login_signup.php with the variables above...
+	else if ($num2 == 1) {
+		echo "Mobile Number Already In Use.";
 	}
+	else {
+		$insert = "INSERT INTO doctors SET first_name = '$first_name', last_name = '$last_name', mobile_number = '$mobile_number', email  = '$email', specialty = '$specialty', password  = '$password'";
+		mysqli_query($connect, $insert);
+		echo "Doctor Registered!"; //fix echo to display on screen
+		
+		//Edit:
+		//hash password before inserting
+		//Display proper ouput that a doctor has been entered
+		//Add admin code so that only admins (of a hospital) can add a doctor and not a random user -> Professional use
+	}
+
+	$insert -> close();
+	$connect -> close();
+	$checker1 -> close();
+	$checker2 -> close();
 ?>
