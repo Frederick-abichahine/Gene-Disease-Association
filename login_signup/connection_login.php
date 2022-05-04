044568f3fc38bf7ad5637a71c9471c477637326d
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-05-04 17:03:08 +0300

    Update connection_login.php

--- login_signup/connection_login.php
+++ login_signup/connection_login.php
@@ -5,17 +5,21 @@
 	mysqli_select_db($connect, 'gene-disease-association-db');
 	$email = $_POST['email'];
 	$password = $_POST['password'];
-	//$error = "Invalid Email or Password.";
-	$select = "SELECT * FROM doctors WHERE email='$email' AND password='$password'";
+	$hash_pass = hash('sha256', $password);
+
+	$error = "Invalid Email or Password.";
+	$select = "SELECT mobile_number, first_name, last_name, specialty FROM doctors WHERE email='$email' AND password='$hash_pass'";
 	$output = mysqli_query($connect, $select);
 	$count = mysqli_num_rows($output);
 
 	if($count == 1){
-		$_SESSION['email']=$email; 
-		header('location:../html/index.html');
+		$_SESSION['email'] = $email;
+		$details = mysqli_fetch_row($output);
+		$_SESSION['details'] = $details; 
+		header('location:../html/index.php');
 	}
 	else{
-		//$_SESSION["error"] = $error;
+		$_SESSION["error"] = $error;
 		header('location:login_signup.php');
 	}
 
